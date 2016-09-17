2aeb40e5d2a60d0094b371d1bf205da71c2c7529
山羊毛 <shanhao2002@gmail.com>
2016-09-17 21:06:35 +0800

    挪位置

--- src/game.py
+++ src/game.py
@@ -1,52 +1,9 @@
 from operator import itemgetter, attrgetter
+from values import AltValue, AddValue
+
 print 'hello'
 
-class AltValue():
-	def __init__(self, v):
-		self.max = v
-		self.v = v
 
-	def __str__(self):
-		return str(self.v)
-
-	def __sub__(self, other):
-		self.v = self._change(-other)
-		return self.v
-
-	def __add__(self, other):
-		self.v = self._change(other)
-		return self.v
-
-	def _change(self, other):
-		v = self.v + other
-		if v > self.max:
-			self.v = self.max
-		elif v < 0:
-			self.v = 0
-		else:
-			self.v = v
-		return self.v
-
-class AddValue():
-	def __init__(self, v):
-		self.v = v
-		self.add = 0
-
-	def __str__(self):
-		return str(self.v + self.add)
-
-	def __sub__(self, other):
-		return self.v + self._change(-other)
-
-	def __add__(self, other):
-		return self.v + self._change(other)
-
-	def _change(self, v):
-		if v < 0 and abs(v) > self.v:
-			self.add = -self.v
-		else:
-			self.add = v
-		return self.add
 
 class Creature():
 	def __init__(self, name, hp, atk, dfs):
@@ -77,17 +34,5 @@ class BattleTime():
 				print u
 			self.units = []
 
-c = Creature("shanhao", 100, 10, 5)
-b = BattleUnit()
-b.activeTime = 14.3
-bb = BattleUnit()
-bb.activeTime = 1.0
-bbb = BattleUnit()
-bbb.activeTime = 5.0
 
-t = BattleTime()
-t.units.append(b)
-t.units.append(bb)
-t.units.append(bbb)
-t.run()
 
---
+++ test.py
@@ -0,0 +1,15 @@
+from src import *
+
+c = game.Creature("shanhao", 100, 10, 5)
+b = game.BattleUnit()
+b.activeTime = 14.3
+bb = game.BattleUnit()
+bb.activeTime = 1.0
+bbb = game.BattleUnit()
+bbb.activeTime = 5.0
+
+t = game.BattleTime()
+t.units.append(b)
+t.units.append(bb)
+t.units.append(bbb)
+t.run()
